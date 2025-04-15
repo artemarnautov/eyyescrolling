8b8a83a0ace1759ce09d430f30490c3a6c54812b
artemarnautov <artemarnautov.uae@gmail.com>
2025-04-15 15:06:03 +0400

    Update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,13 +6,14 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search news..."
-        className="w-full max-w-2xl px-4 py-2 rounded-full bg-[#050f0c]/90 backdrop-blur-md border border-[#53BDEB] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#53BDEB] transition"
+        className="w-full max-w-2xl px-4 py-2 rounded-full bg-[#050f0c]/90 backdrop-blur-md border border-[#1FCFD5] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#1FCFD5] transition"
       />
     </div>
   );
 };
 
 export default SearchBar;
+
 
 
 

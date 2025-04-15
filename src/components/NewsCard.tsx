d91e3b0cfa26b5d64248ab36f543181f614e818a
artemarnautov <artemarnautov.uae@gmail.com>
2025-04-15 14:45:28 +0400

    Update NewsCard.tsx

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChevronDown, ChevronUp, MessageCircle } from 'lucide-react';
 import { NewsItem } from '../types';
 
